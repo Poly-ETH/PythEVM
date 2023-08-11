22671e8b8fe7ae6703d0f1e8c41e06378400903b
Aryan Godara <aryangodara03@gmail.com>
2023-08-12 02:01:11 +0530

    Add main.py
    Signed-off-by: Aryan Godara <aryangodara03@gmail.com>

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+from .src import ExecutionContext, decode_opcode
 
+def run(code: bytes) -> None:
+    """
+    Executes code in a fresh context
+    """
+
+    context = ExecutionContext(code=code)
+
+    while not context.stopped:
+        pc_before = context.pc
+        instruction = decode_opcode(context)
+        instruction.execute(context)
+
+    print(f"{instruction} @ pc={pc_before}")
+    print(context)
+    print()
